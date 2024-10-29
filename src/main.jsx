34f0c8778415683c1264012165d19e0762eb94b7
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-30 01:55:41 +0600

    module 45 started

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello From react router</div>,
+  },
+  {
+    path: '/about',
+    element: <div>I am in the about page</div>
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
